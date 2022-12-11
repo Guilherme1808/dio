b83500f7dd9e1ac335db6074bdd6c66e9ce82799
Guilherme Soares <guilherme1808@hotmail.com>
2022-12-11 15:16:35 -0300

    tela de login com validação para bootcamp da dio

---
+++ tela-login/src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #81259D;
+    color: #FFF;
+
+    border: 1px solid #81259D;
+    border-radius: 21px;
+    
+    &:disabled {
+        pointer-events:${(props) => props.disabled ? 'none' : null};
+        opacity: 0.5;
+        
+    }
+
+    &:hover {
+        opacity: 0.8;
+        cursor:pointer;
+    }
+`
