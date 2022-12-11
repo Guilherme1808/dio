9895aea363fb53e5e38b1a4f399a22190e5c8572
Guilherme Soares <guilherme1808@hotmail.com>
2022-12-11 12:05:03 -0300

    calculadora em react do bootcamp da dio

---
+++ calculadora-react/src/App 2.js
@@ -0,0 +1,15 @@
+import Input from './components/Input'
+import Button from './components/Button'
+import { Container, content } from './styles';
+
+const App = () => {
+  return (
+    <Container>
+      <content>
+        Olá
+      </content>
+    </Container>
+  );
+}
+
+export default App;
---
+++ calculadora-react/src/styles 2.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-color: #FAFAFA;
+
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const content = styled.div`
+background-color: #FFFFFF;
+width: 80%;
+min-height: 350px;
+`
