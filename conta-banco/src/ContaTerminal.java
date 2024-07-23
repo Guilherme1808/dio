03b6b8d1bfa702075248eb2c5c08bb9844495e30
Guilherme Soares <guilherme1808@hotmail.com>
2024-07-23 15:48:58 -0300

    Projeto com fundamentos do Java.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int numero = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o Saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",
+                nomeCliente, agencia, numero, saldo);
+
+        scanner.close();
+
     }
 }
